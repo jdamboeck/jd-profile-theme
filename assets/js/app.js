fc611e4acb52c83f57012eacfb5ffd1592ceaf8c
Johannes Damboeck <j.damboeck@googlemail.com>
2016-08-27 04:27:26 +0200

    Continue work on theme.less and add less-prefix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-
+    $(function() {
+        $('.start').addClass('active');
+            $('.navigation a').click(function() {
+            $('.navigation a').removeClass('active');
+            $(this).addClass('active');
+       });
+    });
 });
